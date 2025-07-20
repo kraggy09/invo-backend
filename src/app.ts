b630ed81f631a0cd6befa28ebc60e585652264ba
Kaif Shaikh <kaifshaikh2013.sk@gmail.com>
2025-07-20 21:34:07 +0530

    CHANGING THE PACKAGE

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
-import chalk from "chalk";
 import userRouter from "./routes/user.route";
 import productRouter from "./routes/product.route";
 import billRouter from "./routes/bill.route";
@@ -20,31 +19,8 @@ app.use(
   })
 );
 
-const colorizeLog = (status: string, log: string) => {
-  if (status.startsWith("404")) return chalk.red.bold(log);
-  if (status.startsWith("2")) return chalk.green.bold(log);
-  if (status.startsWith("4")) return chalk.yellow.bold(log);
-  if (status.startsWith("5")) return chalk.red.bold(log);
-  return chalk.white(log);
-};
-
-morgan.token("body", (req: Request) => JSON.stringify(req.body || {}));
-
-morgan.format("colored", (tokens, req: Request, res: Response) => {
-  const status = tokens.status(req, res) || "000"; // Handle undefined cases
-  const log = [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    status,
-    tokens["response-time"](req, res) + "ms",
-    "- Body:",
-    tokens.body(req, res),
-  ].join(" ");
-
-  return colorizeLog(status, log);
-});
-
-app.use(morgan("colored"));
+// Use morgan's "dev" format for colored logs
+app.use(morgan("dev"));
 
 // Body parsing middleware
 app.use(express.json());
